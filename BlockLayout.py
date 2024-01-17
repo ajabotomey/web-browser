ec9b307d8db800cc707525db107f813dee79b806
ajabotomey <killerbyte_93@hotmail.com>
2024-01-17 17:50:08 +1000

    CSS styles can now be applied (limited for now)

--- BlockLayout.py
+++ BlockLayout.py
@@ -29,15 +29,20 @@ def get_font(size, weight, slant):
 
 
 class DrawText:
-    def __init__(self, x1, y1, text, font):
+    def __init__(self, x1, y1, text, font, color):
         self.top = y1
         self.left = x1
         self.text = text
         self.font = font
+        self.color = color
+
         self.bottom = y1 + font.metrics("linespace")
 
     def execute(self, scroll, canvas):
-        canvas.create_text(self.left, self.top - scroll, text = self.text, font = self.font, anchor = "nw")
+        canvas.create_text(self.left, self.top - scroll, text = self.text, font = self.font, anchor = "nw", fill = self.color)
+
+    def __repr__(self):
+        return "DrawText(text={})".format(self.text)
 
 class DrawRect:
     def __init__(self, x1, y1, x2, y2, color):
@@ -105,15 +110,15 @@ class BlockLayout:
             self.children.append(next)
             previous = next
 
-    def recurse(self, tree):
-        if isinstance(tree, Text):
-            for word in tree.text.split():
-                self.word(word)
+    def recurse(self, node):
+        if isinstance(node, Text):
+            for word in node.text.split():
+                self.word(node, word)
         else:
-            self.open_tag(tree.tag)
-            for child in tree.children:
+            if node.tag == "br":
+                self.flush()
+            for child in node.children:
                 self.recurse(child)
-            self.close_tag(tree.tag)
 
     def open_tag(self, tag):
         if tag == "i":
@@ -150,38 +155,45 @@ class BlockLayout:
         else:
             return "block"
 
-    def word(self, word):
-        font = get_font(self.size, self.weight, self.style)
+    def word(self, node, word):
+        weight = node.style["font-weight"]
+        style = node.style["font-style"]
+        if style == "normal": style = "roman"
+        size = int(float(node.style["font-size"][:-2]) * .75)
+        font = get_font(size, weight, style)
+
         w = font.measure(word)
         if self.cursor_x + w > self.width:
             self.flush()
-        self.line.append((self.cursor_x, word, font))
+        color = node.style["color"]
+        self.line.append((self.cursor_x, word, font, color))
         self.cursor_x += w + font.measure(" ")
 
     def flush(self):
         if not self.line: return
-        metrics = [font.metrics() for x, word, font in self.line]
+        metrics = [font.metrics() for x, word, font, color in self.line]
         max_ascent = max([metric["ascent"] for metric in metrics])
         baseline = self.cursor_y + 1.25 * max_ascent
-        for rel_x, word, font in self.line:
-            x = self.x + rel_x
-            y = self.y + baseline - font.metrics("ascent")
-            self.display_list.append((x, y, word, font))
-        self.cursor_x = 0
+        for x, word, font, color in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font, color))
+        self.cursor_x = self.x
         self.line = []
         max_descent = max([metric["descent"] for metric in metrics])
-        self.cursor_y = baseline + 1.25 + max_descent
+        self.cursor_y = baseline + 1.25 * max_descent
         
     def paint(self):
         cmds = []
-
-        if isinstance(self.node, Element) and self.node.tag == "pre":
+        bgcolor = self.node.style.get("background-color", "transparent")
+        if bgcolor != "transparent":
             x2, y2 = self.x + self.width, self.y + self.height
-            rect = DrawRect(self.x, self.y, x2, y2, "gray")
+            rect = DrawRect(self.x, self.y, x2, y2, bgcolor)
             cmds.append(rect)
 
-        if self.layout_mode() == "inline":
-            for x, y, word, font in self.display_list:
-                cmds.append(DrawText(x, y, word, font))
-
+        for x, y, word, font, color in self.display_list:
+            cmds.append(DrawText(self.x + x, self.y + y, word, font, color))
         return cmds
+    
+    def __repr__(self):
+        return "BlockLayout[{}](x={}, y={}, width={}, height={})".format(
+            self.layout_mode(), self.x, self.y, self.width, self.height)
--- url.py
+++ url.py
@@ -57,6 +57,20 @@ class URL:
         s.close()
 
         return body
+
+    def resolve(self, url):
+        if "://" in url: return URL(url)
+        if not url.startswith("/"):
+            dir, _ = self.path.rsplit("/", 1)
+            while url.startswith("../"):
+                _, url = url.split("/", 1)
+                if "/" in dir:
+                    dir, _ = dir.rsplit("/", 1)
+            url = dir + "/" + url
+        if url.startswith("//"):
+            return URL(self.scheme + ":" + url)
+        else:
+            return URL(self.scheme + "://" + str(self.host) + url)
     
 def show(body):
     in_tag = False
